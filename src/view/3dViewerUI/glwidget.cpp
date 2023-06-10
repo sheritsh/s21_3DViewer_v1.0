b1dced9049bf6c0553271de3e120426743f91f13
sheritsh <sheritsh@student.21-school.ru>
2023-06-10 05:53:49 +0300

    feat: basic UI stylization and maket

--- src/view/3dViewerUI/glwidget.cpp
+++ src/view/3dViewerUI/glwidget.cpp
@@ -14,16 +14,17 @@ void GlWidget::resizeGL(int w, int h)
 
 void GlWidget::paintGL()
 {
+    glClearColor(0, 0, 0, 1);
     glVertexPointer(3, GL_DOUBLE, 0, data.vertices_arr); // number of coordinates per vertex, type of data in array, distance between vertices in array, pointer to array
     glEnableClientState(GL_VERTEX_ARRAY); // enable open gl state
-    glOrtho(-2, 2, -2, 2, 0.1f, 2000);
+    glOrtho(-2, 2, -2, 2, 0.1f, 1000);
     glDrawElements(GL_LINES, data.vertex_indices_count * 2, GL_UNSIGNED_INT, data.vertex_indices_arr); // multiply by two because we draw lines that close
     glDisableClientState(GL_VERTEX_ARRAY);
 }
 
 void GlWidget::parse_obj()
 {
-    char *filename = "/Volumes/sheritsh/C8_3DViewer_v1.0-1/src/tmp/lamp.obj";
+    char *filename = "/Volumes/sheritsh/C8_3DViewer_v1.0-1/src/tmp/skull.obj";
     parse_obj_file(filename, &this->data);
     update();
 //    print_data(&this->data);
--- src/view/3dViewerUI/mainwindow.cpp
+++ src/view/3dViewerUI/mainwindow.cpp
@@ -7,7 +7,18 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->widget->parse_obj();
+    this->setWindowFlags(Qt::WindowType::FramelessWindowHint);
+    this->setAttribute(Qt::WA_TranslucentBackground);
+    int id = QFontDatabase::addApplicationFont(":/res/assets/fonts/SpaceCrusadersItalic-ZV1Zx.ttf");
+    QString family = QFontDatabase::applicationFontFamilies(id).at(0);
+    QFont titleFont(family);
+    int id2 = QFontDatabase::addApplicationFont(":/res/assets/fonts/Montserrat-Regular.ttf");
+    QString family2 = QFontDatabase::applicationFontFamilies(id2).at(0);
+    QFont mainFont(family2);
+    ui->h1->setFont(titleFont);
+    ui->obj_info->setFont(mainFont);
+    this->setAutoFillBackground(true);
+    ui->OpenGlWidget->parse_obj();
 }
 
 MainWindow::~MainWindow()
