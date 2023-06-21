91140baa7b1b9335880b84b65be80e057916a2b3
sheritsh <sheritsh@student.21-school.ru>
2023-06-21 21:10:13 +0300

    fix: modules small fixes

--- src/modules/affine_transformations.h
+++ src/modules/affine_transformations.h
@@ -1,4 +1,20 @@
 #ifndef C8_3DVIEWER_V1_0_1_AFFINE_TRANSFORMATIONS_H
 #define C8_3DVIEWER_V1_0_1_AFFINE_TRANSFORMATIONS_H
 
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "s21_3dviewer.h"
+
+void rotate_X(obj_data *obj, double angle);
+void rotate_Y(obj_data *obj, double angle);
+void rotate_Z(obj_data *obj, double angle);
+
+void scale(obj_data *obj, const double k);
+
+void move_X(obj_data *obj, const double k);
+void move_Y(obj_data *obj, const double k);
+void move_Z(obj_data *obj, const double k);
+
 #endif  // C8_3DVIEWER_V1_0_1_AFFINE_TRANSFORMATIONS_H
